c08fcdb520008e6be03e227bb5808d1a87317a7d
Tu Nombre <tu@email.com>
2025-09-29 09:30:59 -0500

    Feat: Se creó el mapper de SalaFeat: Se creó el mapper de Sala para mapear entre Sala y SalaDTO incluyendo la relación con cineId

---
+++ src/main/java/com/example/parcial2/models/mappers/IMapperSala.java
@@ -0,0 +1,23 @@
+package com.example.parcial2.models.mappers;
+
+import com.example.parcial2.models.Cine;
+import com.example.parcial2.models.Sala;
+import com.example.parcial2.models.dtos.SalaDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IMapperSala {
+
+    //No se mapean los otro atributos, ya que son nativos del modelo Sala
+
+    @Mapping(source = "cine.id", target = "cineId")
+    SalaDTO modeloADto(Sala sala);
+    List<SalaDTO> listaADto(List<Sala> salas);
+
+    @Mapping(source = "cine.id", target = "cineId")
+    Sala dtoAModelo(SalaDTO dto);
+    List<Sala> listaAModelo(List<SalaDTO> dtos);
+}
